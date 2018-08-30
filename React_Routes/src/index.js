70610f4481fd638ff4c3893574637c2963581d07
Roxanne Awang <roxanneawang@hotmail.com>
2018-08-30 19:17:55 -0400

    Router Hw commit

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
-
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
 
-// your code goes here
 class Home extends Component{
   render(){
     return(
-      // your code goes here
-    )
+      <div>
+        
+      <BrowserRouter>
+        <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/guilty">Guilty</Link>
+        </li>
+        <li>
+          <Link to="/happy">Happy</Link>
+        </li>
+        <li>
+          <Link to="/sleepy">Sleepy</Link>
+        </li>
+      </ul>
+
+      <Route path="/guilty" component={Guilty} />
+      <Route path="/happy" component={Happy} />
+      <Route path="/sleepy" component={Sleepy} />
+        </div>
+      </BrowserRouter>
+     </div>
+);
+    
   }
 }
 
